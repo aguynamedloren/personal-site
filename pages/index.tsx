b70ed42cb48f1dec4704cf1a901a3134e5b88a31
Loren Burton <lorendburton@gmail.com>
2022-10-17 16:52:56 -0700

    switch standard to ts-standard

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-black">
+    <div className='flex flex-col items-center justify-center text-black'>
       <Head>
         <title>Loren Burton, Software Engineer</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className='bg-green flex justify-center w-full mt-1'>
@@ -15,39 +15,40 @@ const Home: NextPage = () => {
 
           <div className='w-3/6'>
             <h1 className='text-5xl font-serif mt-7 leading-tight'>
-              Loren Burton,<br/>
+              Loren Burton,<br />
               Software Engineer
             </h1>
 
             <p className='mt-10 pr-16 font-light leading-7'>
-              With over a decade of experience building and growing software 
-              startups from the ground up, I have a proven track record developing 
-              across the tech stack, establishing engineering best practices, and 
-              working with non-technical leaders to execute on business 
+              With over a decade of experience building and growing software
+              startups from the ground up, I have a proven track record developing
+              across the tech stack, establishing engineering best practices, and
+              working with non-technical leaders to execute on business
               requirements.
             </p>
 
             <div className='mt-14'>
-              <a 
-                href='mailto:lorendburton@gmail.com' 
+              <a
+                href='mailto:lorendburton@gmail.com'
                 target='_blank'
                 rel='noreferrer'
-                className='rounded-full bg-green border-black border-2 py-2 px-8 items-center'>
+                className='rounded-full bg-green border-black border-2 py-2 px-8 items-center'
+              >
                 <span className='font-medium'>Email Me</span>
               </a>
             </div>
           </div>
 
           <div className='w-3/6 m-0 p-0'>
-            <Image 
-              src="/profile-photo.png" 
-              alt="Profile photo" 
-              width={445} 
-              height={534} 
+            <Image
+              src='/profile-photo.png'
+              alt='Profile photo'
+              width={445}
+              height={534}
               layout='responsive'
             />
           </div>
-    
+
         </div>
       </main>
     </div>

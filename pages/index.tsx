e01561c29b471c5f62c3775525b04aebdb22c4d1
Loren Burton <lorendburton@gmail.com>
2022-10-17 17:52:29 -0700

    add social icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+const emailUrl = 'mailto:lorendburton@gmail.com'
+const linkedInUrl = 'https://www.linkedin.com/in/aguynamedloren/'
+const githubUrl = 'https://github.com/aguynamedloren'
+const stackoverflowUrl = 'https://stackoverflow.com/users/524659/aguynamedloren'
+
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col items-center justify-center text-black'>
@@ -27,15 +32,43 @@ const Home: NextPage = () => {
               requirements.
             </p>
 
-            <div className='mt-14'>
+            <div className='mt-14 flex gap-3'>
               <a
-                href='mailto:lorendburton@gmail.com'
+                href={emailUrl}
                 target='_blank'
                 rel='noreferrer'
-                className='rounded-full bg-green border-black border-2 py-2 px-8 items-center'
+                className='flex rounded-full bg-green border-black border-2 px-10 items-center h-10 mr-2'
               >
                 <span className='font-medium'>Email Me</span>
               </a>
+
+              <a href={linkedInUrl} className='inline-block'>
+                <Image
+                  src='/icon-linkedin.svg'
+                  alt='LinkedIn'
+                  width={40}
+                  height={40}
+                />
+              </a>
+
+              <a href={githubUrl}>
+                <Image
+                  src='/icon-github.svg'
+                  alt='Github'
+                  width={38}
+                  height={40}
+                />
+              </a>
+
+              <a href={stackoverflowUrl}>
+                <Image
+                  src='/icon-stack-overflow.svg'
+                  alt='Stack Overflow'
+                  width={40}
+                  height={40}
+                />
+              </a>
+
             </div>
           </div>
 
